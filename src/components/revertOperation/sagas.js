60110b8cbf227c6f0fb6cb84a623b78f58b9ac16
TokarAndrii <tokar.andrii@gmail.com>
2019-04-30 21:18:59 +0300

    started doing revertOperations stuff

--- src/components/revertOperation/sagas.js
+++ src/components/revertOperation/sagas.js
@@ -1,12 +1,13 @@
-import { put, call, takeLatest, select } from "redux-saga/effects";
+import { call, takeLatest, select } from "redux-saga/effects";
 import api from "../../utils/api";
-import revertActions from "./revertActions";
+//import revertActions from "./revertActions";
 import revertTypes from "./revertTypes";
 import selectors from "../app/selectors";
 import revertTypesMethods from "../../configs/revertTypes";
 
 function* revertOperation() {
   const method = yield select(selectors.getRevertOperathionMethod);
+
   console.log("method at revertOperation", method);
   //   if (method === revertTypes.DELETE_METHOD_FOR_REVERT) {
   //     console.log("need to call DELETE");
@@ -16,6 +17,12 @@ function* revertOperation() {
       return;
     case revertTypesMethods.DELETE_METHOD_FOR_REVERT:
       console.log("need to call DELETE");
+      const postAdded = yield select(selectors.getAddPost);
+      console.log("postAdded", postAdded);
+      const { id } = postAdded;
+      console.log("id", id);
+      const result = yield call(api.deletePosts, id);
+      console.log("result at function* revertOperation", result);
   }
 }
 
